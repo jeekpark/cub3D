6a63d4a15b7ed12be4ef55742040045964f75e48
Jihyun Kim <ksk208105@gmail.com>
2023-11-19 22:10:51 +0900

    Feat: [include.h] include files

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,13 +6,20 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 04:10:38 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/11/11 04:11:17 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/11/17 12:17:31 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "../mlx/mlx.h"
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include "./struct.h"
 
 
 #endif
