7e3fe7adb52dee1963daa85200619d12acecf3fc
Jihyun Kim <ksk208105@gmail.com>
2023-11-21 22:29:49 +0900

    Add: [fine_wall_from_player.c] init

---
+++ ray_casting/find_wall_from_player.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_wall_from_player.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 16:28:41 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/11/21 22:29:16 by jihykim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+void	find_wall_from_player(t_game *game)
+{
+	t_raycast	*ray;
+
+	ray = &game->ray_info;
+	ray->side = 0;
+	while (TRUE)
+	{
+		if (ray->side_dist.x < ray->side_dist.y)
+		{
+			ray->side_dist.x += ray->delta_dist.x;
+			ray->map_pos.x += ray->step_dir.x;
+			ray->side = W_OR_E;
+		}
+		else
+		{
+			ray->side_dist.y += ray->delta_dist.y;
+			ray->map_pos.y += ray->step_dir.y;
+			ray->side = N_OR_S;
+		}
+		if (game->map[ray->map_pos.y][ray->map_pos.x] != 0)
+			break ;
+	}
+
+
+}
