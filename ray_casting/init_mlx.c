0e6a1f08ed549bb8f8dfe6ddadf336c58d475613
Jihyun Kim <ksk208105@gmail.com>
2023-11-21 14:52:18 +0900

    Modify: [init_mlx.c] rename filename and move init_functions

--- ray_casting/init_mlx.c
+++ ray_casting/init_mlx.c
@@ -1,19 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_drawing_image.c                               :+:      :+:    :+:   */
+/*   init_mlx.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 21:12:21 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/11/21 14:06:17 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/11/21 14:13:57 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void	init_drawing_image(t_game *game)
+void	init_mlx(t_game *game)
 {
+	// get screen image in mlx-screen
+	game->mlx_ptr = mlx_init();
+	game->win_ptr = mlx_new_window(game->mlx_ptr, SCR_WIDTH, SCR_HEIGHT, "cub3D");
+
 	// get image-pointer in mlx-screen
 	game->north.img = mlx_xpm_file_to_image(game->mlx_ptr, \
 		game->img_info.north, &game->north.width, &game->north.height);
@@ -34,3 +38,8 @@ void	init_drawing_image(t_game *game)
 	game->east.addr = mlx_get_data_addr(game->east.img, game->east.bpp, \
 		game->east.line_length, game->east.endian);
 }
+
+// void	get_image_data(t_data *image, void *mlx_ptr)
+// {
+		// for getting image-pointer, pixel-address
+// }
