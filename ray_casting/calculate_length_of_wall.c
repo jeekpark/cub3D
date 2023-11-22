e123c53aaa33966a9ea9dabc763091e1b15b5e6f
Jihyun Kim <ksk208105@gmail.com>
2023-11-22 20:39:34 +0900

    Add: [calculate_length_of_wall.c] 실제 그려야 할 스크린 상의 벽의 길이와, 해당 벽의 텍스쳐가 무엇인지 구하는 함수

---
+++ ray_casting/calculate_length_of_wall.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calculate_length_of_wall.c                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 21:42:02 by jihykim2          #+#    #+#             */
+/*   Updated: 2023/11/22 16:04:52 by jihykim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+void	calculate_length_of_wall(t_game *game)
+{	// 실제 그려야 할 스크린 상의 벽의 길이와, 해당 벽이 어느 방향인지 구하기
+	t_raycast	*ray;
+
+	ray = &game->ray_info;
+	ray->wall_length_in_screen = (int)(SCR_HEIGHT / ray->perp_wall_dist);
+	ray->draw_start = -ray->wall_length_in_screen / 2 + SCR_HEIGHT / 2;
+	if (ray->draw_start < 0)
+		ray->draw_start = 0;
+	ray->draw_end = ray->wall_length_in_screen / 2 + SCR_HEIGHT / 2;
+	if (ray->draw_end >= SCR_HEIGHT)
+		ray->draw_end = SCR_HEIGHT - 1;
+
+	if (ray->side == W_OR_E && ray->ray_dir.x > 0)		// EAST
+		ray->wall_type = EAST;
+	else if (ray->side == W_OR_E && ray->ray_dir.x < 0)	// WEST
+		ray->wall_type = WEST;
+	else if (ray->side == N_OR_S && ray->ray_dir.y > 0)	// NORTH
+		ray->wall_type = NORTH;
+	else if (ray->side == N_OR_S && ray->ray_dir.y < 0)	// SOUTH
+		ray->wall_type = SOUTH;
+}
