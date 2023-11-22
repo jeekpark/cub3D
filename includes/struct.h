aa3545aec2c83344023faf9603973221737ee924
Jihyun Kim <ksk208105@gmail.com>
2023-11-23 02:50:20 +0900

    Chore: [struct.h] t_data -> t_image, t_raycast 내부 변수 추가 및 변경

--- includes/struct.h
+++ includes/struct.h
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 11:54:38 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/11/21 16:38:37 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/11/23 02:37:45 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,19 @@
 # define SCR_HEIGHT	900
 # define BUFFER_SIZE 42
 
-typedef struct	s_vec_f
+typedef struct s_vec_f
 {
 	double	x;
 	double	y;
 }	t_vec_f;
 
-typedef struct	s_vec_i
+typedef struct s_vec_i
 {
 	int	x;
 	int	y;
 }	t_vec_i;
 
-typedef struct s_data
+typedef struct s_image
 {
 	void	*img;
 	char	*addr;
@@ -56,7 +56,7 @@ typedef struct s_data
 	int		endian;
 	int		width;
 	int		height;
-}	t_data;
+}	t_image;
 
 typedef struct s_texture
 {
@@ -68,6 +68,14 @@ typedef struct s_texture
 	int		ceiling;	// ex) 0xFF0000
 }	t_texture;
 
+typedef enum e_direction
+{
+	NORTH = 2,
+	SOUTH,
+	WEST,
+	EAST,
+}	t_direction;
+
 typedef struct s_raycast
 {
 	t_vec_f	ray_dir;
@@ -78,15 +86,14 @@ typedef struct s_raycast
 	t_vec_i	step_dir;
 
 	int		side;
-}	t_raycast;
+	int		perp_wall_dist;
+	int		wall_length_in_screen;
+	int		draw_start;
+	int		draw_end;
+	double	hit_point;		// hit_point at wall-texture(real pixel value of x)
 
-typedef enum e_direction
-{
-	NORTH = 2,
-	SOUTH,
-	WEST,
-	EAST,
-}	t_direction;
+	t_image	*wall_data;		// copy address of t_game's t_texture
+}	t_raycast;
 
 /* [parse -> execute]: 넘겨줄 인자 */
 // 이들 모두 2차원 평면 상으로 가정하므로 값 방향 유의하기
@@ -105,11 +112,11 @@ typedef struct s_game
 
 	void		*mlx_ptr;
 	void		*win_ptr;
-	t_data		screen;
-	t_data		north;
-	t_data		south;
-	t_data		west;
-	t_data		east;
+	t_image		screen;
+	t_image		north;
+	t_image		south;
+	t_image		west;
+	t_image		east;
 
 }	t_game;
 
