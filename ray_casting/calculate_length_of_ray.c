e0c7cdbf999c3b5bce2c29bbd3d5d3e9ad3798a2
Jihyun Kim <ksk208105@gmail.com>
2023-11-23 11:48:00 +0900

    Fix: [calculate_length_of_ray.c] error in value of perp_wall_dist

--- ray_casting/calculate_length_of_ray.c
+++ ray_casting/calculate_length_of_ray.c
@@ -6,7 +6,7 @@
 /*   By: jihykim2 <jihykim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 21:39:19 by jihykim2          #+#    #+#             */
-/*   Updated: 2023/11/23 11:07:05 by jihykim2         ###   ########.fr       */
+/*   Updated: 2023/11/23 11:42:59 by jihykim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,16 @@ void	calculate_length_of_ray(t_game *game)
 	ray = &game->ray_info;
 	if (ray->side == W_OR_E)
 	{
-		// ray->perp_wall_dist = ray->side_dist.x - ray->delta_dist.x;
-		ray->perp_wall_dist = (ray->map_pos.x - game->player.x + \
-			(1 - ray->step_dir.x) / 2) / ray->ray_dir.x;
+		ray->perp_wall_dist = ray->side_dist.x - ray->delta_dist.x;
 		ray->hit_point = game->player.y + \
 			ray->perp_wall_dist * ray->ray_dir.y;
 	}
 	else
 	{
-		// ray->perp_wall_dist = ray->side_dist.y - ray->delta_dist.y;
-		ray->perp_wall_dist = (ray->map_pos.y - game->player.x + \
-			(1 - ray->step_dir.x) / 2) / ray->ray_dir.x;
+		ray->perp_wall_dist = ray->side_dist.y - ray->delta_dist.y;
 		ray->hit_point = game->player.x + \
 			ray->perp_wall_dist * ray->ray_dir.x;
 	}
 	ray->hit_point -= floor(ray->hit_point);
-	printf("%f\n", ray->hit_point);
+	// printf("%f\n", ray->hit_point);
 }
